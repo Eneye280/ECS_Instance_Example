2943c77870c47c0889d0211f96d08339c5ba9e6c
Euclides <eneye280@gmail.com>
2019-12-02 09:41:05 -0500

    v1.0 ECS Function Instance Cube
    * Add Instance of Cube with ECS

---
+++ ECS_Instance_Example/Assets/ECS Instance Cube/Script/ECSTest.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ECSTest : MonoBehaviour
+{
+    public int howManyMeshInstantiator;
+    public float distanceMeshWithMesh;
+    public Mesh m_cubeMesh;
+    public Material[] m_cubeMaterials;
+
+    private EntityManager m_entityManager;
+    private EntityArchetype m_CubeArchetype;
+
+    private void Start()
+    {
+        m_entityManager = World.Active.EntityManager;
+        m_CubeArchetype = m_entityManager.CreateArchetype(
+            typeof(RenderMesh),
+            typeof(Translation),
+            typeof(LocalToWorld)
+            );
+
+        CreateCube();
+    }
+
+    private void CreateCube()
+    {
+
+        NativeArray<Entity> nativeArray = new NativeArray<Entity>(howManyMeshInstantiator, Allocator.Temp);
+
+        m_entityManager.CreateEntity(m_CubeArchetype, nativeArray);
+
+        foreach (var entity in nativeArray)
+        {
+            int randomMaterialIndex = UnityEngine.Random.Range(0, m_cubeMaterials.Length);
+
+            m_entityManager.SetComponentData(entity, new Translation
+            {
+                Value = new float3(UnityEngine.Random.Range(-distanceMeshWithMesh, distanceMeshWithMesh), 
+                0, UnityEngine.Random.Range(-distanceMeshWithMesh, distanceMeshWithMesh))
+            }); ;
+
+            m_entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = m_cubeMesh,
+                material = m_cubeMaterials[randomMaterialIndex]
+            });
+        }
+    }
+} 
+
+
